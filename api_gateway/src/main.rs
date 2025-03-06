419187b2b2de91cf2403c937b3dd15c0ffba1afc
Felopater-Melika <felopatermelika@gmail.com>
2025-03-05 19:57:27 -0600

    changes

--- api_gateway/src/main.rs
+++ api_gateway/src/main.rs
@@ -1,12 +1,14 @@
+use anyhow::Result;
 use axum::{Extension, Router, routing::get};
 use echourl::hello_world_server::{HelloWorld, HelloWorldServer};
 use echourl::{Greet, Greeted};
+use std::env;
 use tokio::sync::oneshot;
 use tonic::{Request, Response, Status, transport::Server};
 use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
 use tower_http::trace::{self, TraceLayer};
-use tracing::Level;
+use tracing::{Level, error, info};
 
 mod echourl {
     tonic::include_proto!("echourl");
@@ -18,7 +20,7 @@ struct HelloWorldService {}
 #[tonic::async_trait]
 impl HelloWorld for HelloWorldService {
     async fn greeter(&self, request: Request<Greet>) -> Result<Response<Greeted>, Status> {
-        println!("Got a request: {:?}", request);
+        info!("Received gRPC request: {:?}", request);
 
         let response = Greeted {
             greeted: "Hello, World from gRPC!".to_string(),
@@ -31,51 +33,54 @@ impl HelloWorld for HelloWorldService {
 struct State {}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt::init();
+async fn main() -> Result<()> {
+    unsafe {
+        env::set_var("RUST_LOG", "debug");
+    }
 
-    let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
 
     let http_server = tokio::spawn(async move {
         let app = Router::new().route("/", get(root)).layer(
             ServiceBuilder::new()
                 .layer(Extension(State {}))
                 .layer(CompressionLayer::new())
-                .layer(
-                    TraceLayer::new_for_http()
-                        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
-                        .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-                ),
+                .layer(TraceLayer::new_for_http()),
         );
 
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-        println!("🚀 HTTP server listening on 0.0.0.0:3000");
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+        info!("🚀 HTTP server listening on 0.0.0.0:3000");
 
-        axum::serve(listener, app).await.unwrap();
+        axum::serve(listener, app).await?;
+        Ok::<(), anyhow::Error>(())
     });
 
     let grpc_server = tokio::spawn(async move {
-        let addr = "0.0.0.0:50051".parse().unwrap();
+        let addr = "0.0.0.0:50051".parse()?;
         let service = HelloWorldService::default();
 
-        println!("🚀 gRPC server listening on 0.0.0.0:50051");
+        info!("🚀 gRPC server listening on 0.0.0.0:50051");
 
         Server::builder()
             .add_service(HelloWorldServer::new(service))
             .serve_with_shutdown(addr, async {
                 shutdown_rx.await.ok();
             })
-            .await
-            .unwrap();
+            .await?;
+        Ok::<(), anyhow::Error>(())
     });
 
     tokio::select! {
         _ = http_server => {
-            println!("HTTP server stopped!");
+            error!("HTTP server stopped!");
             let _ = shutdown_tx.send(());
         }
         _ = grpc_server => {
-            println!("gRPC server stopped!");
+            error!("gRPC server stopped!");
         }
     }
 
--- shortner_service/src/main.rs
+++ shortner_service/src/main.rs
@@ -1,24 +1,30 @@
+use anyhow::Result;
+use echourl::Greet;
 use echourl::hello_world_client::HelloWorldClient;
-use echourl::{Greet, Greeted};
-use tonic::transport::Server;
-use tonic::{Request, Response, Status};
+use tonic::Request;
+use tracing::{error, info};
 
 mod echourl {
     tonic::include_proto!("echourl");
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = HelloWorldClient::connect("http://0.0.0.0:50051").await?;
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
 
-    println!("🚀 gRPC Client Requesting 0.0.0.0:50051");
+    let mut client = HelloWorldClient::connect("http://0.0.0.0:50051").await?;
+    info!("🚀 gRPC Client Requesting 0.0.0.0:50051");
 
     let request = tonic::Request::new(Greet {
         greet: "Hello from gRPC Client!".to_string(),
     });
 
-    let response = client.greeter(request).await?;
-    println!("gRPC Response: {:?}", response.into_inner().greeted);
+    match client.greeter(request).await {
+        Ok(response) => info!("✅ gRPC Response: {:?}", response.into_inner().greeted),
+        Err(e) => error!("❌ gRPC Request failed: {}", e),
+    }
 
     Ok(())
 }
